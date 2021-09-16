3a0d476991a1776c8b672319b4dff6d0d6917d01
Davever1005 <davidchong@ever-technologies.com>
2021-09-16 22:34:52 +0800

    init and first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+
+with open("file.txt", "r") as f:
+    a = f.readline()  # starts as a string
+    a = int(a)
+    if a == "":
+        a = 0
+    elif a >= 100:
+        a = 0
+
+# check if its an empty string, otherwise should be able to cast using int()
+
+if st.button("Click me"):
+    a += 1
+    with open("file.txt", "w") as f:
+        f.truncate()
+        f.write(f"{a}")
+        st.write(a)
+        st.progress(a)
